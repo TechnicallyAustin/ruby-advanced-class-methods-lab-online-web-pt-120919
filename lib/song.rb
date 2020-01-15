ed3a925b638f0bb345872016f83c8980d651bf5a
Austin Johnson <technicallyaustin@gmail.com>
2020-01-15 21:22:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -66,7 +66,12 @@ class Song
     end
     
   def self.create_from_filename(name)
-    self.new_from_filename(name)
+    song_arr = filename.split(" - ")
+    song_arr[1] = song_arr[1].chomp(".mp3")
+    song = self.new
+    song.name = song_arr[1]
+    song.artist_name = song_arr[0]
+    song 
     Song.save 
    
     end
