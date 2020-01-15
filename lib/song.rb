67b606532e8f594a3778d97fa29c4ed1e050072a
Austin Johnson <technicallyaustin@gmail.com>
2020-01-15 21:17:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -66,9 +66,8 @@ class Song
     end
     
   def self.create_from_filename(name)
-    self.new_from_filename(name)
-    self.save 
-
+   song = self.create
+   self.new_from_filename
     end
     
   #def self.destroy_all
